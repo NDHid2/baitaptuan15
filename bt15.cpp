fe3dfe91a9bc1608e314b9d1df9eef3dc8d90101
Hiep <ndhiep0404@gmail.com>
2026-06-25 01:29:19 +0700

    them ham main

--- bt15.cpp
+++ bt15.cpp
@@ -69,3 +69,59 @@ int bfsShortestPath(int a[][N], int start, int target, vector<int>& path)
 
     return path.size() - 1;   // so canh = so dinh - 1
 }
+
+int main()
+{
+    int a[N][N] = {0};
+
+    
+    themCanh(a, 0, 1);   // Ha Noi - Son Tay
+    themCanh(a, 0, 2);   // Ha Noi - Hoa Binh
+    themCanh(a, 0, 3);   // Ha Noi - Phu Ly
+    themCanh(a, 0, 5);   // Ha Noi - Hai Duong
+    themCanh(a, 0, 9);   // Ha Noi - Bac Ninh
+    themCanh(a, 0, 10);  // Ha Noi - Thai Nguyen
+
+    themCanh(a, 3, 4);   // Phu Ly - Hung Yen
+    themCanh(a, 4, 5);   // Hung Yen - Hai Duong
+
+    themCanh(a, 5, 6);   // Hai Duong - Hai Phong
+
+    themCanh(a, 6, 7);   // Hai Phong - Uong Bi
+
+    themCanh(a, 7, 8);   // Uong Bi - Bac Giang
+    themCanh(a, 8, 9);   // Bac Giang - Bac Ninh
+
+    themCanh(a, 9, 7);   // Bac Ninh - Uong Bi
+
+    string startName, targetName;
+    cout << "Nhap ten diem bat dau: ";
+    getline(cin, startName);
+    cout << "Nhap ten diem ket thuc: ";
+    getline(cin, targetName);
+
+    // Tim chi so tuong ung trong danh sach
+    int start = -1, target = -1;
+    for (int i = 0; i < N; i++)
+    {
+        if (city[i] == startName) start = i;
+        if (city[i] == targetName) target = i;
+    }
+
+    if (start == -1 || target == -1)
+    {
+        cout << "Khong tim thay dia diem!\n";
+        return 0;
+    }
+
+    // Tim duong di ngan nhat
+    vector<int> path;
+    int soCanh = bfsShortestPath(a, start, target, path);
+
+    if (soCanh == -1)
+        cout << "Khong co duong di!\n";
+    else
+        cout  << "duong di ngan nhat la " <<soCanh << endl;   
+
+    return 0;
+}
